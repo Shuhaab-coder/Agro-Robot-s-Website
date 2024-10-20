1e22353027a70bcad3fa85587d2be11d7fe5c60f
Shuhaab-coder <zarger546@gmail.com>
2024-10-20 20:34:47 +0530

    Added More Things To CSS

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
-const AboutBtn = document.getElementById('Aboutusbtn');
-const hideAboutUsBtn = document.getElementById('hideAboutUsBtn');
-const aboutText = document.querySelector('.about-text');
+const AboutBtn = document.getElementById("Aboutusbtn");
+const hideAboutUsBtn = document.getElementById("hideAboutUsBtn");
+const aboutText = document.querySelector(".about-text");
 
 AboutBtn.onclick = () => {
-    AboutBtn.classList.add('hidden');
-    aboutText.classList.toggle('hidden');
-    hideAboutUsBtn.classList.remove('hidden');
-}
+  AboutBtn.classList.add("hidden");
+  aboutText.classList.toggle("hidden");
+  hideAboutUsBtn.classList.remove("hidden");
+};
 
 hideAboutUsBtn.onclick = () => {
-    AboutBtn.classList.remove('hidden');
-    aboutText.classList.add('hidden');
-    hideAboutUsBtn.classList.add('hidden');
-}
+  AboutBtn.classList.remove("hidden");
+  aboutText.classList.add("hidden");
+  hideAboutUsBtn.classList.add("hidden");
+};
